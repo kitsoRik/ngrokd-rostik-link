0b3a9caaec4d7fd3d7b25ad99c0431695fd125e4
Rostyslav Pidburachynskyi <=>
2021-06-28 13:38:27 +0300

    Add default server addr to client

--- src/ngrok/client/model.go
+++ src/ngrok/client/model.go
@@ -21,7 +21,7 @@ import (
 )
 
 const (
-	defaultServerAddr   = "localhost:4443"
+	defaultServerAddr   = "ngrokd.rostik.link:80"
 	defaultInspectAddr  = "127.0.0.1:4040"
 	pingInterval        = 20 * time.Second
 	maxPongLatency      = 15 * time.Second
